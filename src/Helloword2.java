9b82b7364250236416ab96d96b38a4151cedcec0
liuchuan <liu_chuan1@hoperun.com>
2017-09-26 14:56:37 +0800

    Initial commit

---
+++ src/Helloword2.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/9/26.
+ */
+public class Helloword2 {
+    public static void main(String[] args){
+        System.out.print("");
+        Map<String,Object> map = new HashMap<String,Object>();
+    }
+}
